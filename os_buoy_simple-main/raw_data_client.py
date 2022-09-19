9250a1c3672c950f58a9f0cd7149b98009c63e35
CT-Hung <poilkj09862@gmail.com>
2022-09-19 16:28:01 +0800

    1. server can response the data of real wav file. 2. raw_data_client.py can show the time series data

---
+++ os_buoy_simple-main/raw_data_client.py
@@ -0,0 +1,77 @@
+import matplotlib
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+#from utils.connect import tryRequestPost
+import requests
+
+import argparse
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+f, ax = plt.subplots()
+fs = 96000
+data_buffer_sec = 10
+data_buffer = np.zeros(fs*data_buffer_sec)
+x = np.arange(fs*data_buffer_sec)/fs
+payload = {}
+
+def getParser():
+    parser = argparse.ArgumentParser(description='my description')
+    parser.add_argument('-ip', '--ip', default='127.0.0.1')
+    parser.add_argument('--delay', default=1, type=float)
+    return parser
+
+
+def animate(url, delay):
+    def update(i):
+        global payload
+        global data_buffer
+        global fs
+        global x
+        r = requests.post(url, json=payload)
+        response_json = r.json()
+        response_size = response_json[0]['record']
+        print("Get data size:", response_size)
+        if response_size != 0:
+            data = pd.DataFrame(response_json[1:1+response_size])
+
+            if fs != data['fs'].iloc[-1]:
+                fs = data['fs'].iloc[-1]
+                data_buffer = np.zeros(fs*data_buffer_sec)
+                x = np.arange(fs*data_buffer_sec)/fs
+
+            data_buffer_tmp = np.zeros(fs*response_size)
+            for i in range(response_size):
+                if i == 0:
+                    data_buffer_tmp[-(i+1)*fs:] = data['data'].iloc[i]
+                else:
+                    data_buffer_tmp[-(i+1)*fs:-i*fs] = data['data'].iloc[i]
+
+            data_buffer[-(data_buffer_sec-response_size)*fs:] = data_buffer[0:(data_buffer_sec-response_size)*fs]
+            data_buffer[0:response_size*fs] = data_buffer_tmp
+            time_stamp = data['time_stamp'].iloc[-1]
+            payload = {'time_stamp':time_stamp}
+            print("Last time stamp => ", time_stamp)
+
+        print("----------------------------------------")
+        
+
+        ax.clear()
+        ax.plot(x, data_buffer, '-')
+        ax.set_xlabel("Time (sec)")
+        ax.set_ylabel("Volt")
+
+
+    ani = animation.FuncAnimation(f, update, interval=delay*1000)
+    plt.show()
+
+if __name__ == '__main__':
+    parser = getParser()
+    args = parser.parse_args()
+    url = "http://"+args.ip+":8000/raw_data/"
+    animate(url, args.delay)
+
+
--- os_buoy_simple-main/simplebuoy_server_db_test.py
+++ os_buoy_simple-main/simplebuoy_server_db_test.py
@@ -11,27 +11,37 @@ import numpy as np
 from typing import Optional
 from datetime import datetime, timezone, timedelta
 import random
+import soundfile as sf
+from pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
 id_number = 0
+wav_file_i = 0
 url = "127.0.0.1"
 return_time_stamp = datetime.now(timezone.utc)
+fs = 64000
+
+class RawData(BaseModel):
+    time_stamp: datetime = None
+
 
 @app.get("/connect_pamguard/")
 async def getConnection():#連接測試
-    fs = 51200
+    #fs = 51200
     return {'status':'success','fs':fs}
 
+
 @app.post("/raw_data/")
-async def getPamGuardData(time_stamp: Optional[datetime]=None):#存進來的資料
-    print("[RECEIVE] time_stamp : ", time_stamp);
-    global id_number, return_time_stamp
-    fs = 51200
-    t = np.array(range(0, fs))/fs
-    freq = 10
-    amp = 0.5
-    data = np.sin(2*np.pi*freq*t)*amp
+async def getPamGuardData(raw_data_arg: RawData):#存進來的資料
+    print("[RECEIVE] time_stamp : ", raw_data_arg.time_stamp)
+    global id_number, return_time_stamp, wav_file_i
+
+    #fs = 51200
+    #t = np.array(range(0, fs))/fs
+    #freq = 10
+    #amp = 0.5
+    #data = np.sin(2*np.pi*freq*t)*amp
 
     record = int(random.uniform(0, 4))
     print("[SENT] Amount of return values: ", record)
@@ -42,9 +52,16 @@ async def getPamGuardData(time_stamp: Optional[datetime]=None):#存進來的資�
         for i in range(record):
             return_time_stamp += timedelta(seconds=1)
             id_number += 1
+            
+            if wav_file_i >= np.size(y)/fs:
+                wav_file_i = 0  # restart wav file
+
+            data = np.flip(y[wav_file_i*fs:(wav_file_i+1)*fs]) #output data need to be flipped
+            wav_file_i += 1
             returnValue = {'id': id_number, 'time_stamp':return_time_stamp.isoformat("T", "milliseconds"), 'fs': fs, 'name':'test_data', 'data':data.tolist()}
             print("Return List => ", returnValue['time_stamp'])
             returnList.append(returnValue)
+            print("Wav i => ", wav_file_i)
         # print("last time stamp: ", return_time_stamp.isoformat("T", "milliseconds"))
        
         return returnList
@@ -52,4 +69,6 @@ async def getPamGuardData(time_stamp: Optional[datetime]=None):#存進來的資�
 
 
 if __name__ == "__main__":
+    wav_file = "HY_20210825_123724.wav"
+    y, fs = sf.read(wav_file)
     uvicorn.run(app=app, host="127.0.0.1", port=8000, log_level="info")
